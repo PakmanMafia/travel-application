bfb4013f7ea13b3721a16da05a0af9e0001fb951
ness <duranr@protonmail.com>
2022-12-12 20:28:41 -0500

    Red Cube Test Complete

--- src/3/script.js
+++ src/3/script.js
@@ -1,1 +1,29 @@
-console.log('Hello Three.js')
+// Scene
+const scene = new THREE.Scene()
+const viewport_dimension = {
+    width: 800,
+    height: 600
+}
+
+// Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xeb1313 })
+const mesh = new THREE.Mesh(geometry, material)
+
+// Camera
+const aspectRatio = viewport_dimension.width / viewport_dimension.height
+const camera = new THREE.PerspectiveCamera(75, aspectRatio)
+
+// Scene Add
+scene.add(mesh)
+scene.add(camera)
+
+// Renderer
+camera.position.y = 1
+camera.position.z = 3
+const canvas = document.querySelector('canvas.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(viewport_dimension.width, viewport_dimension.height)
+renderer.render(scene, camera)
