de62a25cf23b2755895c5527dfaff026c0f4407c
ness <duranr@protonmail.com>
2022-12-19 21:00:39 -0500

    Working on final project for learning HTMl and CSS

--- src/5/src/script.js
+++ src/5/src/script.js
@@ -1,75 +1,32 @@
 import './style.css'
 import * as THREE from 'three'
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
 // Scene
 const scene = new THREE.Scene()
-
-/**
- * Objects
- */
-
-const group = new THREE.Group()
-scene.add(group)
-
-const cube_one = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-
-const cube_two = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-)
-
-group.add(cube_one)
-group.add(cube_two)
-
-cube_one.position.set(-1, 1, -0.5)
-cube_two.position.set(1, -1, 0.5)
-group.position.set(0, 0.25, 0)
-group.rotation.set(Math.PI / 2, 0, 0, 'XYZ')
-
-/**
- * Sizes
- */
-const sizes = {
+const viewport_dimension = {
     width: 800,
     height: 600
 }
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+// Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xeb1313 })
+const mesh = new THREE.Mesh(geometry, material)
+
+// Camera
+const aspectRatio = viewport_dimension.width / viewport_dimension.height
+const camera = new THREE.PerspectiveCamera(75, aspectRatio)
+
+// Scene Add
+scene.add(mesh)
 scene.add(camera)
 
-/*
-    Transform
-*/
-// Axes Helpter
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
-
-// Position
-camera.position.set(0, 0, 3)
-// mesh.position.set(0.7, -0.6, 1)
-
-// // Scale
-// mesh.scale.set(2, 0.5, 0.5)
-
-// // Rotation
-// mesh.rotation.set(Math.PI / 2, Math.PI / 2, Math.PI / 2, 'YXZ')
-
-// Look-At
-// camera.lookAt(new THREE.Vector3(-0.15, -0.5, 2))
-/**
- * Renderer
- */
+// Renderer
+camera.position.y = 1
+camera.position.z = 3
+const canvas = document.querySelector('canvas.webgl')
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-renderer.setSize(sizes.width, sizes.height)
+renderer.setSize(viewport_dimension.width, viewport_dimension.height)
 renderer.render(scene, camera)
